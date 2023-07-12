cf8992cec92f62bbd096b67337ace1bd93cb35f8
lcgwhat <348437207@qq,com>
2023-07-12 16:32:19 +0800

    单元测试的setUp

--- app/Behavior/demo/Tests/LeaveServiceNewTest.php
+++ app/Behavior/demo/Tests/LeaveServiceNewTest.php
@@ -10,21 +10,27 @@ use PHPUnit\Framework\TestCase;
 class LeaveServiceNewTest extends TestCase
 {
     private LeaveServiceNew $service;
-    public function __construct($name = null, array $data = [], $dataName = '')
+    protected function setUp(): void
     {
         $this->service =  new LeaveServiceNew();
-        parent::__construct($name, $data, $dataName);
     }
+
+
 
     function testCase1(){
 
         $form = new LeaveForm(new Employee('李二', 1), '婚嫁', 1, 0);
         $this->service->audit($form);
-        var_dump(1);die;
+       // $this->getActualOutput();
+        $this->assertEquals(1,1);
+       // var_dump(1);die;
     }
     function testCase(){
         $form = new LeaveForm(new Employee('黄部长', 9), '世家', 10, 2);
         $this->service->audit($form);
-        die;
+
+       $this->getActualOutput();
+
+      //  die;
     }
 }
